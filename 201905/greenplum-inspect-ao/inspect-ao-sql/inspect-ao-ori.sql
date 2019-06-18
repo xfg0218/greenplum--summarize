1121d981b8b56f6a08aada596df9f222c624edcd
xiaoxu <1234@qq.com>
2019-06-18 10:35:04 +0800

    删除之前的版本

--- 201905/greenplum-inspect-ao/inspect-ao-sql/inspect-ao-ori.sql
+++
@@ -1,1 +0,0 @@
-SELECT ns.nspname,pc.relname FROM pg_class pc,pg_namespace ns WHERE pc.relnamespace = ns.oid AND relstorage IN ('c', 'a') AND ns.nspname = 'schemaName';
--- 201905/greenplum-inspect-ao/inspect-ao-sql/inspect-ao-percent-hidden-ori.sql
+++
@@ -1,1 +0,0 @@
-select  percent_hidden from gp_toolkit.__gp_aovisimap_compaction_info('tablename'::regclass) ORDER BY percent_hidden desc limit 1;
